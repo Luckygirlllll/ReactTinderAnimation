f46225fea28e2a9509cf9e98207be10fc7d9e92b
Iryna <irynakanivets@gmail.com>
2017-09-12 12:31:29 +0300

    added all files

--- App.js
+++ App.js
@@ -1,12 +1,73 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Ball from './src/Ball';
+import {Card, Button} from "react-native-elements";
+import Deck from './src/Deck';
+
+const DATA = [
+  { id: 1, text: 'Card #1', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 2, text: 'Card #2', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 3, text: 'Card #3', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 4, text: 'Card #4', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+  { id: 5, text: 'Card #5', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 6, text: 'Card #6', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 7, text: 'Card #7', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 8, text: 'Card #8', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+];
+
 
 export default class App extends React.Component {
+
+
+  renderCard(item){
+      return(
+          <Card
+              key = {item.id}
+              title={item.text}
+              image={{uri:item.uri}}
+          >
+          <Text style= { {marginBottom:10} }>
+          I can customise card further!
+          </Text>
+          <Button
+            icon = { {name: 'code'} }
+            backgroundColor = "#03A9F4"
+            title = "View Now!"
+
+
+          />
+          </Card>
+      );
+  }
+
+renderNoMoreCards(){
+    return(
+        <Card title="All Done!" >
+          <Text style={ {marginBottom:10} } >
+            There is no more content here!
+          </Text>
+          <Button
+              backgroundColor="#03A9F4"
+              title = "Get more!"
+          />
+
+        </Card>
+
+    );
+
+}
+
+
+
+
   render() {
     return (
       <View style={styles.container}>
-        <Ball />
+        <Deck
+        data = {DATA}
+        renderCard = {this.renderCard}
+        renderNoMoreCards= {this.renderNoMoreCards}
+        onSwipeRight = { () => console.log("something was swipped")}
+        />
       </View>
     );
   }
